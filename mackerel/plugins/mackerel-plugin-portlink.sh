9983ddc4c82c4181e402f9ea75c1ca66e09f2d1c
INAGAKI Hiroshi <musashino.open@gmail.com>
2017-09-20 00:40:20 +0900

    mackerel-agent: Change the linkspeed for down interfaces

--- mackerel/plugins/mackerel-plugin-portlink.sh
+++ mackerel/plugins/mackerel-plugin-portlink.sh
@@ -6,15 +6,16 @@ SWNAME='portlink.switch0.'
 PORTS=`for i in $(seq 0 5) ;do uci -q get network.@switch_vlan[$i].ports ; done | grep -oE "[0-9][^t]"`
 
 if [ -n "$MACKEREL_AGENT_PLUGIN_META" ]; then
-     echo -e "# mackerel-agent-plugin\n{\"graphs\":{\"portlink.switch0\":{\"label\":\"Ethernet LinkSpeed (switch0)\",\"unit\":\"integer\",\"metrics\":[{\"name\":\"*\",\"label\":\"%1\",\"stacked\":false}]}}}"
+     echo -e "# mackerel-agent-plugin\n{\"graphs\":{\"portlink.switch0\":{\"label\":\"Ethernet LinkSpeed on switch0 (Mbps)\",\"unit\":\"integer\",\"metrics\":[{\"name\":\"*\",\"label\":\"%1\",\"stacked\":false}]}}}"
 else
      for p in $PORTS; do
           NAME=${SWNAME}port$p
           LINK_STATUS=`swconfig dev switch0 port $p get link | grep -oE "link:(\w+)" | grep -oE "(down|up)"`
           LINK_SPEED=`swconfig dev switch0 port $p get link | grep -oE "speed:(\d+)" | grep -oE "\d+"`
 
-          if [ "$LINK_STATUS" = "up" ]; then
-               echo -e "${SWNAME}port$p\t${LINK_SPEED}\t${SECONDS}"
+          if [ "$LINK_STATUS" = "down" ]; then
+               LINK_SPEED=0
           fi
+          echo -e "${SWNAME}port$p\t${LINK_SPEED}\t${SECONDS}"
      done
 fi
