742bd7dc54c38dd0607eb5051709353cf82b91ef
INAGAKI Hiroshi <musashino.open@gmail.com>
2017-09-20 00:15:26 +0900

    mackerel-agent: Add graph defines for portlink plugin

--- mackerel/plugins/mackerel-plugin-portlink.sh
+++ mackerel/plugins/mackerel-plugin-portlink.sh
@@ -2,16 +2,19 @@
 # ref: https://github.com/ymyzk/mackerel-plugins/blob/master/raspberrypi/temp.sh
 SECONDS=`date '+%s'`
 
-SWNAME='switch0.'
+SWNAME='portlink.switch0.'
 PORTS=`for i in $(seq 0 5) ;do uci -q get network.@switch_vlan[$i].ports ; done | grep -oE "[0-9][^t]"`
 
-for p in $PORTS; do
-     NAME=${SWNAME}port$p
-     LINK_STATUS=`swconfig dev switch0 port $p get link | grep -oE "link:(\w+)" | grep -oE "(down|up)"`
-     LINK_SPEED=`swconfig dev switch0 port $p get link | grep -oE "speed:(\d+)" | grep -oE "\d+"`
+if [ -n "$MACKEREL_AGENT_PLUGIN_META" ]; then
+     echo -e "# mackerel-agent-plugin\n{\"graphs\":{\"portlink.switch0\":{\"label\":\"Ethernet LinkSpeed (switch0)\",\"unit\":\"integer\",\"metrics\":[{\"name\":\"*\",\"label\":\"%1\",\"stacked\":false}]}}}"
+else
+     for p in $PORTS; do
+          NAME=${SWNAME}port$p
+          LINK_STATUS=`swconfig dev switch0 port $p get link | grep -oE "link:(\w+)" | grep -oE "(down|up)"`
+          LINK_SPEED=`swconfig dev switch0 port $p get link | grep -oE "speed:(\d+)" | grep -oE "\d+"`
 
-     if [ "$LINK_STATUS" = "up" ]; then
-          echo -e "${SWNAME}port$p\t${LINK_SPEED}\t${SECONDS}"
-     fi
-done
-#echo -e "${NAME}\t${VALUE}\t${SECONDS}"
+          if [ "$LINK_STATUS" = "up" ]; then
+               echo -e "${SWNAME}port$p\t${LINK_SPEED}\t${SECONDS}"
+          fi
+     done
+fi
