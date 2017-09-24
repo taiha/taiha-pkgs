34923608d1b6dae123cf73e09baabffb7d135180
INAGAKI Hiroshi <musashino.open@gmail.com>
2017-09-24 16:14:46 +0900

    mackerel-agent: Change the methods to collect protocol stats

--- mackerel/plugins/mackerel-plugin-appproto.sh
+++ mackerel/plugins/mackerel-plugin-appproto.sh
@@ -3,8 +3,11 @@ SECONDS=`date '+%s'`
 
 PLUGIN_NAME='appproto.'
 
+# Load json tool
+. /usr/share/libubox/jshn.sh
+
 PROTO="ICMP IGMP FTP SSH Telnet SMTP DNS HTTP POP2 POP3 NTP NetBIOS IMAP IMAP3"
-PROTO="${PROTO} HTTPS SMBoverTCP FTPS IMAPS POP3S BitTorrent GRE ESP IPIP L2TPv3 null"
+PROTO="${PROTO} HTTPS SMBoverTCP FTPS IMAPS POP3S BitTorrent GRE ESP IPIP L2TPv3 Other"
 for app in $PROTO; do
 	eval rxBytes_"${app}"='0'
 	eval rxPkts_"${app}"='0'
@@ -12,44 +15,50 @@ for app in $PROTO; do
 	eval txPkts_"${app}"='0'
 done
 
-NLBW_TEXT=$(nlbw -c json | sed 's/\"data\":\[/@/' | sed 's/\],\[/\]*\[/g' )
+# Load json from nlbw
+NLBW_JSON=$(nlbw -c json)
+json_load $NLBW_JSON
 
-IFS='@'
-set -- $NLBW_TEXT
+# Select "data" field
+json_select data
 
-DATA_STR=$2
-IFS='*'
-i=0
+# Get rows in data
+json_get_keys rows
 
-for data in $DATA_STR; do
-	DATA_ROW=$(echo $data | tr -d '[]}\"')
+for row in $rows; do
+	# Select row in data
+	json_select row
+	# Get items in row
+	json_get_keys items
+	# Get values in row
+	json_get_var nlbw_proto 11	# application protocol
+	json_get_var rx_bytes 7		# rx bytes
+	json_get_var rx_pkts 8		# rx packets
+	json_get_var tx_bytes 9		# tx bytes
+	json_get_var tx_pkts 10		# tx packets
+	# Return to upper level
+	json_select ..
 
-	IFS=','
-	set -- $DATA_ROW
-	nlbw_proto=$(echo $11 | tr -d '-')
-	rx_bytes=$7
-	rx_pkts=$8
-	tx_bytes=$9
-	tx_pkts=$10
-	IFS=' '
-	for app in $PROTO; do
-		if [ "$app" = "$nlbw_proto" ]; then
-		eval rxBytes_${app}=$(( rxBytes_${app} + $rx_bytes))
-		eval rxPkts_${app}=$(( rxPkts_${app} + $rx_pkts))
-		eval txBytes_${app}=$(( txBytes_${app} + $tx_bytes))
-		eval txPkts_${app}=$(( txPkts_${app} + $tx_pkts))
-		fi
-	done
+	# Cut dash letter in $nlbw_proto if it's value is "SMB-over-TCP"
+	if [ "$nlbw_proto" = "SMB-over-TCP" ]; then
+		nlbw_proto="SMBoverTCP"
+	fi
+	# Set "Other" to $nlbw_proto if it's value is null
+	if [ "$nlbw_proto" = "" ]; then
+		nlbw_proto="Other"
+	fi
+
+	eval rxBytes_${nlbw_proto}=$(( rxBytes_${nlbw_proto} + $rx_bytes))
+	eval rxPkts_${nlbw_proto}=$(( rxPkts_${nlbw_proto} + $rx_pkts))
+	eval txBytes_${nlbw_proto}=$(( txBytes_${nlbw_proto} + $tx_bytes))
+	eval txPkts_${nlbw_proto}=$(( txPkts_${nlbw_proto} + $tx_pkts))
 done
 
 for app in $PROTO; do
-	rxBytes=$(eval echo \"\$rxBytes_${app}\")
+	rxBytes=$(eval echo \"\$rxBytes_$app\")
 	rxPkts=$(eval echo \"\$rxPkts_$app\")
 	txBytes=$(eval echo \"\$txBytes_$app\")
 	txPkts=$(eval echo \"\$txPkts_$app\")
-	if [ "$app" = "null" ]; then
-		app="Other"
-	fi
 	if [ "$rxBytes" != "0" ] || [ "$rxPkts" != "0" ] || [ "$txBytes" != "0" ] || [ "$txPkts" != "0" ] ; then
 		echo -e "${PLUGIN_NAME}${app}.rxBytes\t${rxBytes}\t${SECONDS}"
 		echo -e "${PLUGIN_NAME}${app}.rxPkts\t${rxPkts}\t${SECONDS}"
