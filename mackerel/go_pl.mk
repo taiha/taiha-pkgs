ce5d92dd078f2bc9873b00a1e2fa68b3364a15de
INAGAKI Hiroshi <musashino.open@gmail.com>
2019-06-19 22:41:34 +0900

    mackerel-agent: fix indentations in Makefiles

--- mackerel/go_pl.mk
+++ mackerel/go_pl.mk
@@ -8,86 +8,86 @@ PLUGIN_VER:=0.56.0
 PLUGIN_TAR:=v$(PLUGIN_VER).tar.gz
 
 define Package/mackerel-plugin-conntrack
-	$(call Package/mackerel-agent/Default)
-	TITLE:=conntrack plugin for mackerel-agent
-	DEPENDS:=mackerel-agent
+$(call Package/mackerel-agent/Default)
+  TITLE:=conntrack plugin for mackerel-agent
+  DEPENDS:=mackerel-agent
 endef
 
 define Package/mackerel-plugin-conntrack/description
-	nf_conntrack custom metrics
+nf_conntrack custom metrics
 endef
 
 define Package/mackerel-plugin-linux
-	$(call Package/mackerel-agent/Default)
-	TITLE:=linux plugin for mackerel-agent
-	DEPENDS:=mackerel-agent +ss +coreutils +coreutils-who
+$(call Package/mackerel-agent/Default)
+  TITLE:=linux plugin for mackerel-agent
+  DEPENDS:=mackerel-agent +ss +coreutils +coreutils-who
 endef
 
 define Package/mackerel-plugin-linux
-	Get linux process metrics
+Get linux process metrics
 endef
 
 define Package/mackerel-plugin-multicore
-	$(call Package/mackerel-agent/Default)
-	TITLE:=multicore plugin for mackerel-agent
-	DEPENDS:=mackerel-agent
+$(call Package/mackerel-agent/Default)
+  TITLE:=multicore plugin for mackerel-agent
+  DEPENDS:=mackerel-agent
 endef
 
 define Package/mackerel-plugin-multicore/description
-	Get multicore CPU metrics
+Get multicore CPU metrics
 endef
 
 define Package/mackerel-plugin-uptime
-	$(call Package/mackerel-agent/Default)
-	TITLE:=uptime plugin for mackerel-agent
-	DEPENDS:=mackerel-agent
+$(call Package/mackerel-agent/Default)
+  TITLE:=uptime plugin for mackerel-agent
+  DEPENDS:=mackerel-agent
 endef
 
 define Package/mackerle-plugin-uptime/description
-	uptime custom metrics
+uptime custom metrics
 endef
 
 define Download/mackerel-official-plugins
-	URL:=https://github.com/mackerelio/mackerel-agent-plugins/archive/
-	FILE:=$(PLUGIN_TAR)
-	HASH:=2deb9892038a2739539ddaf9854bd6016c1c7c65e915b04941be4820a35fd7f8
+  URL:=https://github.com/mackerelio/mackerel-agent-plugins/archive/
+  FILE:=$(PLUGIN_TAR)
+  HASH:=2deb9892038a2739539ddaf9854bd6016c1c7c65e915b04941be4820a35fd7f8
 endef
 
 define Download/mackerel-plugin-conntrack
-	$(call Download/mackerel-official-plugins)
+$(call Download/mackerel-official-plugins)
 endef
 $(eval $(call Download,mackerel-plugin-conntrack))
 
 define Download/mackerel-plugin-linux
-	$(call Download/mackerel-official-plugins)
+$(call Download/mackerel-official-plugins)
 endef
 $(eval $(call Download,mackerel-plugin-linux))
 
 define Download/mackerel-plugin-multicore
-	$(call Download/mackerel-official-plugins)
+$(call Download/mackerel-official-plugins)
 endef
 $(eval $(call Download,mackerel-plugin-multicore))
 
 define Download/mackerel-plugin-uptime
-	$(call Download/mackerel-official-plugins)
+$(call Download/mackerel-official-plugins)
 endef
 $(eval $(call Download,mackerel-plugin-uptime))
 
 # plugin installations
 define Package/mackerel-plugin-conntrack/install
-	$(call mackerel-plugins/install,conntrack)
+$(call mackerel-plugins/install,conntrack)
 endef
 
 define Package/mackerel-plugin-linux/install
-	$(call mackerel-plugins/install,linux)
+$(call mackerel-plugins/install,linux)
 endef
 
 define Package/mackerel-plugin-multicore/install
-	$(call mackerel-plugins/install,linux)
+$(call mackerel-plugins/install,linux)
 endef
 
 define Package/mackerel-plugin-uptime/install
-	$(call mackerel-plugins/install,uptime)
+$(call mackerel-plugins/install,uptime)
 endef
 
 # Plugin packages
--- mackerel/script_pl.mk
+++ mackerel/script_pl.mk
@@ -7,46 +7,46 @@
 SCRIPT_PLUGINS:= temp portlink appproto
 
 define Package/mackerel-plugin-temp
-	$(call Package/mackerel-agent/Default)
-	TITLE:=temp plugin for mackerel-agent
-	DEPENDS:=mackerel-agent
+$(call Package/mackerel-agent/Default)
+  TITLE:=temp plugin for mackerel-agent
+  DEPENDS:=mackerel-agent
 endef
 
 define Package/mackerel-plugin-temp/description
-	Get CPU/SoC temperature
+Get CPU/SoC temperature
 endef
 
 define Package/mackerel-plugin-portlink
-	$(call Package/mackerel-agent/Default)
-	TITLE:=portlink plugin for mackerel-agent
-	DEPENDS:=mackerel-agent
+$(call Package/mackerel-agent/Default)
+  TITLE:=portlink plugin for mackerel-agent
+  DEPENDS:=mackerel-agent
 endef
 
 define Package/mackerel-plugin-portlink/description
-	Get link speed of ethernet ports on the switch
+Get link speed of ethernet ports on the switch
 endef
 
 define Package/mackerel-plugin-appproto
-	$(call Package/mackerel-agent/Default)
-	TITLE:=appproto plugin for mackerel-agent
-	DEPENDS:=mackerel-agent +nlbwmon
+$(call Package/mackerel-agent/Default)
+  TITLE:=appproto plugin for mackerel-agent
+  DEPENDS:=mackerel-agent +nlbwmon
 endef
 
 define Package/mackerel-plugin-appproto/description
-	Get Bytes/Packets of application protocols
+Get Bytes/Packets of application protocols
 endef
 
 # package installations
 define Package/mackerel-plugin-temp/install
-	$(call mackerel-plugins/install,temp)
+$(call mackerel-plugins/install,temp)
 endef
 
 define Package/mackerel-plugin-portlink/install
-	$(call mackerel-plugins/install,portlink)
+$(call mackerel-plugins/install,portlink)
 endef
 
 define Package/mackerel-plugin-appproto/install
-	$(call mackerel-plugins/install,appproto)
+$(call mackerel-plugins/install,appproto)
 endef
 
 # Plugin packages
