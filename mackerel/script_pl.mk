daa4d0ff08c07a8752b19be86bc6875a4ad12e6d
INAGAKI Hiroshi <musashino.open@gmail.com>
2019-06-19 14:01:58 +0900

    mackerel-agent: rewrite/split Makefiles

---
+++ mackerel/script_pl.mk
@@ -0,0 +1,55 @@
+#
+# Copyright (C) 2017 musashino205
+#
+# This is free software, licensed under the GNU General Public License v2.
+#
+
+SCRIPT_PLUGINS:= temp portlink appproto
+
+define Package/mackerel-plugin-temp
+	$(call Package/mackerel-agent/Default)
+	TITLE:=temp plugin for mackerel-agent
+	DEPENDS:=mackerel-agent
+endef
+
+define Package/mackerel-plugin-temp/description
+	Get CPU/SoC temperature
+endef
+
+define Package/mackerel-plugin-portlink
+	$(call Package/mackerel-agent/Default)
+	TITLE:=portlink plugin for mackerel-agent
+	DEPENDS:=mackerel-agent
+endef
+
+define Package/mackerel-plugin-portlink/description
+	Get link speed of ethernet ports on the switch
+endef
+
+define Package/mackerel-plugin-appproto
+	$(call Package/mackerel-agent/Default)
+	TITLE:=appproto plugin for mackerel-agent
+	DEPENDS:=mackerel-agent +nlbwmon
+endef
+
+define Package/mackerel-plugin-appproto/description
+	Get Bytes/Packets of application protocols
+endef
+
+# package installations
+define Package/mackerel-plugin-temp/install
+	$(call mackerel-plugins/install,temp)
+endef
+
+define Package/mackerel-plugin-portlink/install
+	$(call mackerel-plugins/install,portlink)
+endef
+
+define Package/mackerel-plugin-appproto/install
+	$(call mackerel-plugins/install,appproto)
+endef
+
+# Plugin packages
+$(eval $(call BuildPackage,mackerel-plugin-temp)
+$(eval $(call BuildPackage,mackerel-plugin-portlink)
+$(eval $(call BuildPackage,mackerel-plugin-appproto)
